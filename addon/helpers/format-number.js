26d4e5b96b4d866ad0b334a4685cfb40dee3aad9
Troy Nichols <troygnichols@gmail.com>
2015-09-28 12:02:31 -0500

    Don't pass undefined to numeral.js

--- addon/helpers/format-number.js
+++ addon/helpers/format-number.js
@@ -2,8 +2,22 @@ import Ember from 'ember';
 import numeral from 'numeral';
 
 export function formatNumber(params, hash) {
-	let { format } = hash || {};
-	return numeral(params).format(format);
+  let { format } = hash || {};
+  let number = params;
+
+  if (Ember.isArray(params)) {
+    number = params[0];
+  }
+
+  if (typeof(number) === 'undefined') {
+    number = null;
+  }
+
+  if (isNaN(number)) {
+    number = null;
+  }
+
+  return numeral(number).format(format);
 }
 
 export default Ember.Helper.helper(formatNumber);
