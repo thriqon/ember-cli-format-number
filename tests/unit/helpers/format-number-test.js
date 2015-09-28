932bb0de970e4dab9e8f79cb89f378f4f2011f85
Troy Nichols <troygnichols@gmail.com>
2015-09-28 12:02:31 -0500

    Don't pass undefined to numeral.js
    closes #1

--- tests/unit/helpers/format-number-test.js
+++ tests/unit/helpers/format-number-test.js
@@ -10,3 +10,15 @@ test('it renders 7884486213 as 7.3GB', function (assert) {
 test('it renders 1230974 as 1.2m', function (assert) {
 	assert.equal(formatNumber(1230974, {format: '0.0a'}), '1.2m');
 });
+
+test('it renders [1000] as 1,000', function(assert) {
+  assert.equal(formatNumber([1000], {format: '0,0'}), '1,000');
+});
+
+test('it renders undefined as 0', function(assert) {
+  assert.equal(formatNumber(undefined), '0');
+});
+
+test('it renders NaN as 0', function(assert) {
+  assert.equal(formatNumber(NaN), 0);
+});
